4c046083d9f4d4845b12a701ac9bd6f1e48c0b0b
sal400 <flarebd@gmail.com>
2024-03-05 01:35:21 +0600

    category modified

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -4,19 +4,19 @@ const Category = () => {
             <div>
                 <div className="my-14 gap-5 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 w-full">
                     <div className="p-5 bg-green rounded-lg flex flex-col justify-center items-center">
-                        <img src="https://i.ibb.co/SVLQTMc/plate-04.png" />
+                        <img src="https://i.ibb.co/SVLQTMc/plate-04.png" className="py-4" />
                         Appetizer
                     </div>
                     <div className="p-5 bg-green rounded-lg flex flex-col justify-center items-center">
-                        <img src="https://i.ibb.co/0cqfCt6/plate-02.png" />
+                        <img src="https://i.ibb.co/0cqfCt6/plate-02.png" className="py-4" />
                         Main Dish
                     </div>
                     <div className="p-5 bg-green rounded-lg flex flex-col justify-center items-center">
-                        <img src="https://i.ibb.co/R7mjqrs/plate-03.png" />
+                        <img src="https://i.ibb.co/R7mjqrs/plate-03.png" className="py-4" />
                         Signature Dish
                     </div>
                     <div className="p-5 bg-green rounded-lg flex flex-col justify-center items-center">
-                        <img src="https://i.ibb.co/tz94hbD/plate-01.png" />
+                        <img src="https://i.ibb.co/tz94hbD/plate-01.png" className="py-4" />
                         Dessert
                     </div>
                 </div>
