97525c6677c7d1a6024a6a5ebfbfe7d96e4819a9
sal400 <flarebd@gmail.com>
2024-03-04 18:29:04 +0600

    Category section done

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,12 +1,24 @@
 const Category = () => {
     return (
-        <div className="text-white text-center text-2xl font-semibold uppercase">
+        <div className="text-white text-2xl font-bold">
             <div>
                 <div className="my-14 gap-5 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 w-full">
-                    <div className="p-5 bg-green rounded-lg">box-1</div>
-                    <div className="p-5 bg-green rounded-lg">box-2</div>
-                    <div className="p-5 bg-green rounded-lg">box-3</div>
-                    <div className="p-5 bg-green rounded-lg">box-4</div>
+                    <div className="p-5 bg-green rounded-lg flex flex-col justify-center items-center">
+                        <img src="https://i.ibb.co/SVLQTMc/plate-04.png" />
+                        Appetizer
+                    </div>
+                    <div className="p-5 bg-green rounded-lg flex flex-col justify-center items-center">
+                        <img src="https://i.ibb.co/0cqfCt6/plate-02.png" />
+                        Main Dish
+                    </div>
+                    <div className="p-5 bg-green rounded-lg flex flex-col justify-center items-center">
+                        <img src="https://i.ibb.co/R7mjqrs/plate-03.png" />
+                        Signature Dish
+                    </div>
+                    <div className="p-5 bg-green rounded-lg flex flex-col justify-center items-center">
+                        <img src="https://i.ibb.co/tz94hbD/plate-01.png" />
+                        Dessert
+                    </div>
                 </div>
             </div>
         </div>
