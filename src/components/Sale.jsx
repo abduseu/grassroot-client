47b5ac5f0aae54c36384f0f248af8398f4193596
sal400 <flarebd@gmail.com>
2024-03-07 18:14:45 +0600

    Promo section modified

--- src/components/Sale.jsx
+++ src/components/Sale.jsx
@@ -1,9 +1,10 @@
 const Sale = () => {
     return (
-        <div className="bg-green text-white my-14 rounded-lg text-center text-2xl font-semibold uppercase">
+        <div className="bg-black text-white my-14 md:p-12 rounded-lg text-center">
             <div>
                 <div className="p-5 gap-5 w-full">
-                    10% off today
+                    <h3 className="text-2xl font-black uppercase pb-4">10% off today</h3>
+                    <p className="font-thin text-lg">Treat yourself to our mouthwatering menu of delectable dishes, made with the finest ingredients and crafted to perfection. Whether you're craving savory classics or tempting treats, there's something for everyone to enjoy. Don't miss out on this limited-time opportunity to savor incredible flavors at unbeatable prices. Hurry and place your order now to enjoy 10% off today!</p>
                 </div>
             </div>
         </div>
